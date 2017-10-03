220e3c731bec918451b191fb3d024a4ca01933ed
Cian (Surface Pro) <cian.byrne@coliemore.com.au>
2017-10-03 19:21:01 +1100

    test serial messenger

---
+++ wdriver/ser-messenger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import serial
+
+# update to person you are sending to:
+ser = serial.Serial('COM5', 9600, timeout=3)
+
+while True:
+    data = ser.read()
+    print(data)
+    din = input("Please enter a message: ")
+    ser.write(din)
+
+ser.close()
