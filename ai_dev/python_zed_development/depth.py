91e5ed99f372147575d26c6158a9990bc28fa6c6
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2017-10-13 13:43:06 +1100

    moved training data to folder

--- ai_dev/python_zed_development/depth.py
+++ ai_dev/python_zed_development/depth.py
@@ -26,12 +26,12 @@ def main():
 
     # Capture 50 images and depth, then stop
     i = 0
-    num_images=5
+    num_images=60
     image = core.PyMat()
     depth_for_display = core.PyMat()
 
-    print('Current mode: Capture 5 images as fast as possible.\nDo not merge the images.\nSave everything to pickle files.')
-
+    print('Current mode: Capture {} images as fast as possible.\nMerge the images.\nSave to pickle files.'.format(num_images))
+    start_time=time.time()
     while i < num_images:
         # A new image is available if grab() returns PySUCCESS
         if zed.grab(runtime_parameters) == tp.PyERROR_CODE.PySUCCESS:
@@ -61,22 +61,23 @@ def main():
 
             # Save Images
             # start_time=time.time()
-            pickle.dump(data,open( 'image_rgb{}.pickle'.format(i), 'wb' ))
-            pickle.dump(depth_data,open( 'image_depth{}.pickle'.format(i), 'wb' ))
+            # pickle.dump(data,open( 'image_rgb{}.pickle'.format(i), 'wb' ))
+            # pickle.dump(depth_data,open( 'image_depth{}.pickle'.format(i), 'wb' ))
             # elapsed_time=time.time()-start_time
             # print('pickle time taken = {}'.format(elapsed_time))
 
             # Merge images
             # start_time=time.time()
-            # merged = merge_images(data,depth_data)
+            merged = merge_images(data,depth_data)
             # elapsed_time=time.time()-start_time
             # print('merge time taken = {}'.format(elapsed_time))
-            # pickle.dump(merged,open( 'image_merged{}.pickle'.format(i), 'wb' ))
+            pickle.dump(merged,open( 'dataset/image_merged{}.pickle'.format(i), 'wb' ))
         else:
             print('image collection failed')
         # Increment the loop
         i = i + 1
     print('Image capture complete')
+    print('Total time taken = {}'.format(time.time()-start_time))
     # Close the camera
     zed.close()
 
@@ -158,10 +159,10 @@ def check_merge():
     ax.imshow(merged)
     plt.show()
 
-# main()
+main()
 # load_and_display("image_depth0.pickle")
-check_merge()
+# check_merge()
 
-import statistics
-times = [load_and_merge(i) for i in range(20)]
-print('avg time: {0:.4f}s'.format(statistics.mean(times)))
+# import statistics
+# times = [load_and_merge(i) for i in range(20)]
+# print('avg time: {0:.4f}s'.format(statistics.mean(times)))
