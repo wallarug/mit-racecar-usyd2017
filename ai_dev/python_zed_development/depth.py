e480223ed09ff49c5c16e439e9bf0f761476002d
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2017-10-15 09:39:08 +1100

    Added script to convert stuff to image files. Should use a better save function in future

--- ai_dev/python_zed_development/depth.py
+++ ai_dev/python_zed_development/depth.py
@@ -180,7 +180,41 @@ def get_image_filename(index):
         raise FileNotFoundError("No filename found with index of {}".format(index))
         return ''
 
-main(60) #capture 60 images
+def save_all_images():
+    import cv2
+    import pickle
+    import os
+
+    # saves all the files in the dataset folder as image files in a different folder
+    image_folder= 'images/'
+
+    # all files
+    prefix='image_'
+    prefixed = [filename for filename in os.listdir('dataset') if filename.startswith(prefix)]
+
+    for image_pickle_file in prefixed:
+        image = pickle.load( open( 'dataset/'+image_pickle_file, "rb" ) )
+
+        red, green, blue, depth = cv2.split(image)
+        rgb_image = cv2.merge((red,green,blue))
+        depth_image = cv2.merge((depth,depth,depth))
+
+        image_name = image_pickle_file.replace('.pickle','')
+
+        print('saving image {}'.format(image_name))
+
+        save_image(image_folder+'rgb/'+image_name , rgb_image)
+        save_image(image_folder+'depth/'+image_name , depth_image)
+
+def save_image(filename,image):
+    from matplotlib import pyplot as plt
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+    plt.savefig(filename+'.png', bbox_inches='tight')
+    plt.close()
+
+# main(60) #capture 60 images
+save_all_images()
 # load_and_display(0)
 # load_and_display(0,'rgb')
 # load_and_display(0,'depth')
