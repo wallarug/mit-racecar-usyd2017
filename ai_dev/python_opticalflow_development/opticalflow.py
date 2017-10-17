36afeea953a43387d42747c9fa94f2d0cc55c1cf
AJ <ajackass@AJs-MacBook-Pro.local>
2017-10-17 22:18:14 +1100

    further optical flow improvements, edge detection

--- ai_dev/python_opticalflow_development/opticalflow.py
+++ ai_dev/python_opticalflow_development/opticalflow.py
@@ -10,7 +10,7 @@
 ### http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
 ### https://stackoverflow.com/questions/35372700/whats-0xff-for-in-cv2-waitkey1
 
-### this needs optimisation. And to have the optimisation tested.
+### this needs optimisation & testing on the CNN.
 
 import numpy as np
 import cv2
@@ -20,14 +20,14 @@ cap = cv2.VideoCapture(0)
 
 ## detection of corners
 # params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 5, #100
-                      qualityLevel = 0.01, #0.9
-                      minDistance = 7, #7
-                      blockSize = 7 ) #7
+feature_params = dict( maxCorners = 150, # Display #num of frames (Dot Points)
+                      qualityLevel = 0.3,
+                      minDistance = 5,
+                      blockSize = 50 ) # Small window is more sensitive to noise and may miss larger motions
 
-##
+## using lk of
 # Parameters for lucas kanade optical flow
-lk_params = dict( winSize  = (15,15),
+lk_params = dict( winSize  = (15,15),  #15 x 15 set pixel dimensions
                  maxLevel = 2,
                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
@@ -38,8 +38,6 @@ p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
-#mask = np.zeros_like(0)
-
 
 ## we may want to compare each old frame to the previous
 while(1):
@@ -59,35 +57,41 @@ while(1):
         c,d = old.ravel()
         mask = cv2.line(mask, (a,b),(c,d), (103, 209, 51), 6) ## visual line which is connected to the dot point
         frame = cv2.circle(frame,(a,b),5, (255, 255, 255),2) ## visual circle dot point
-    img = cv2.add(frame,mask) ## adds the dot and line together into an image
+        img = cv2.add(frame, 0) ## adds the dot and line together into an image
 
     cv2.imshow('frame',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
 
-#these just show the different images that get saved.
 
+#these just show the different images that get saved.
 #    from matplotlib import pyplot as plt
-#   fig, ax = plt.subplots()
-#    ax.imshow(mask)
+#    fig, ax = plt.subplots()
+#    ax.imshow(img)
 #    plt.savefig('test.png', bbox_inches='tight')
 #    plt.close()
 #
-#   fig, ax = plt.subplots()
-#   ax.imshow(frame)
-#   plt.savefig('test2.png', bbox_inches='tight')
-#   plt.close()
+#    fig, ax = plt.subplots()
+#    ax.imshow(frame)
+#    plt.savefig('test2.png', bbox_inches='tight')
+#    plt.close()
 
-    # Now update the previous frame and previous points
-    #old_gray = frame_gray.copy()
-    old_gray = frame_gray.copy() - cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+#    fig, ax = plt.subplots()
+#    ax.imshow(old_gray)
+#    plt.savefig('test.png', bbox_inches='tight')
+#    plt.close()
 
-    #p0 = good_new.reshape(-1,1,2)
 
-    #still not working, however below gives an iterative attempt every frame to re-calculate new points to detect.
-    #issue when all points leave the frame, program crashed. thus there always needs to be a frame on the screen.
+##  OG update the previous frame and previous points
+    old_gray = frame_gray.copy()
+
+##  NEW refresh each frame
     p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+
+    #issue when all points leave the frame, program crashes.
+    #thus there always needs to be a frame on the screen.
+
 
 
 
