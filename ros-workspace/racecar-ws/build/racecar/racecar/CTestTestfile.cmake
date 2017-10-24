37f754d5d018c8dcdb6088d777a9bef77be48288
Cian Byrne <wallarug@bitbucket.org>
2017-10-24 14:43:30 +1100

    initial commit of working racecar-ws workspace.  This is modified so that the zed_wrapper library works and that there are no errors generated by the missing LIDAR or IMU

---
+++ ros-workspace/racecar-ws/build/racecar/racecar/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nvidia/Documents/racecar-ws/src/racecar/racecar
+# Build directory: /home/nvidia/Documents/racecar-ws/build/racecar/racecar
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
