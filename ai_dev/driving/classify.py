86497edeff116b8f77e2430edc62768ca9e8b66f
Gengxing Wang <gwan8849@uni.sydney.edu.au>
2017-10-25 14:07:34 +0000

    classify.py edited online with Bitbucket

--- ai_dev/driving/classify.py
+++ ai_dev/driving/classify.py
@@ -9,5 +9,5 @@ def evaluate_one(pickle_file, model_path):
     pickle_file = pickle_file.reshape((1, pickle_file.shape[0], pickle_file.shape[1], pickle_file.shape[2]))
     
     class_index = np.argmax(model.predict(pickle_file))
-    steering_value = class_index/30.0
+    steering_value = ((class_index / 5.0) + ((class_index + 1) / 5.0)) / 2
     return steering_value
