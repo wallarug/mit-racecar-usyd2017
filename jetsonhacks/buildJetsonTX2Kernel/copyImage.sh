05f07135e48d2a99537e29a1479e73e60b9c3992
Cian Byrne <wallarug@bitbucket.org>
2017-10-28 01:53:22 +1100

    REPO CLEAN UP: ros_integration.  Removed files.

--- jetsonhacks/buildJetsonTX2Kernel/copyImage.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo ./scripts/copyImage.sh
--- mit-racecar/hardware/installXpad.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install git
-sudo apt-get install dkms
-sudo git clone https://github.com/paroj/xpad.git /usr/src/xpad-0.4
-sudo dkms install -m xpad -v 0.4
-sudo modprobe xpad
