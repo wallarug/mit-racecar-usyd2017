4f40add728b819e291278915d1be0f8827a94b1c
Cian Byrne <wallarug@bitbucket.org>
2017-10-28 01:37:26 +1100

    removed all racecar-ws folder

--- ros-workspace/racecar-ws/build/zed_wrapper/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/Documents/racecar-ws/src/zed_wrapper
-# Build directory: /home/nvidia/Documents/racecar-ws/build/zed_wrapper
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
